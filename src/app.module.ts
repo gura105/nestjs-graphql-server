57936ab3bd7ab0139ff11731502b103c06d153f4
togo-yamanaka <togo.yamanaka@ariseanalytics.com>
2022-08-14 20:59:12 +0900

    add dynamic provider that influences mater module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,34 @@
 import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
+import { PrismaModule } from '@pb-components/prisma/prisma.module';
 import * as path from 'path';
+import { WinstonModule } from 'nest-winston';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostsModule } from './components/posts/posts.module';
-
+import { PostsModule } from '@pb-components/posts/posts.module';
+import { PbEnvModule } from '@pb-config/environments/pb-env.module';
+import { PbEnv } from '@pb-config/environments/pb-env.service';
 
 @Module({
-  imports: [GraphQLModule.forRoot({
-    autoSchemaFile: path.join(process.cwd(), "src/schema.gql"),
-    sortSchema: true,
-  }), 
-  PostsModule,
+  imports: [
+    GraphQLModule.forRootAsync({
+      inject: [PbEnv],
+      useFactory: (env: PbEnv) => env.GqlModuleOptionsFactory,
+    }),
+    WinstonModule.forRootAsync({
+      inject: [PbEnv],
+      useFactory: (env: PbEnv) => env.WinstonModuleOptionsFactory,
+    }),
+    PrismaModule.forRootAsync({
+      imports: [WinstonModule],
+      inject: [PbEnv],
+      isGlobal: true,
+      useFactory: (env: PbEnv) => ({
+        prismaOptions: env.PrismaOptionsFactory,
+      }),
+    }),
+    PbEnvModule,
+    PostsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/components/posts/post.resolvers.ts
+++ src/components/posts/post.resolvers.ts
@@ -1,21 +1,34 @@
 import { Args, Query, Resolver } from '@nestjs/graphql';
-import { PostModel } from './interfaces/post.model';
+import { PbEnv } from '@pb-config/environments/pb-env.service';
+import { PostModel } from '@pb-components/posts/interfaces/post.model';
+import { ConfigService } from '@nestjs/config';
 
 @Resolver((of) => PostModel)
 export class PostsResolver {
-  constructor() {}
+    constructor(
+        private configService: ConfigService,
+        private pbEnv: PbEnv
+    ) {}
 
-  @Query(() => [PostModel], { name: 'posts', nullable: true })
-  async getPosts() {
-    return [
-      {
-        id: '1',
-        title: 'NestJS is so good.',
-      },
-      {
-        id: '2',
-        title: 'GraphQL is so good.',
-      },
-    ];
-  }
+    @Query(() => [PostModel], { name: 'posts', nullable: true })
+    async getPosts() {
+        return [
+            {
+                id: '1',
+                title: 'NestJS is so good.',
+            },
+            {
+                id: '2',
+                title: 'GraphQL is so good.',
+            },
+        ];
+    }
+    @Query(()=>String, {name: 'helloConfiguration'})
+    helloConfiguration() :number{
+        return this.configService.get<number>('PORT') // 3333 (number型になります)
+    }
+    @Query(()=>String)
+    helloEnv(): string{
+        return this.pbEnv.DatabaseUrl;
+    }
 }
